536596aed283e8c2ae80fcd46756fdde47a9ea5a
tominaga <aurora25sh.50nonono@gmail.com>
2022-04-27 11:21:39 +0900

    ファイル情報をインスタンス変数に代入

--- 08.ls_object/lib/Ls/file.rb
+++ 08.ls_object/lib/Ls/file.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
+require 'etc'
+
 module Ls
   class File
     attr_reader :name
 
     def initialize(name)
       @name = name
+      fs = ::File::Stat.new(name)
+      @ftype = fs.ftype
+      @mode = fs.mode.to_s(8)
+      @nlink = fs.nlink
+      @user_name = Etc.getpwuid(fs.uid).name
+      @group_name = Etc.getgrgid(fs.gid).name
+      @size = fs.size
+      @mtime = fs.size
     end
 
     def name_length
