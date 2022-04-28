0f1f8ef943fb3abd3f482877dd77345e60921664
tominaga <aurora25sh.50nonono@gmail.com>
2022-04-28 11:46:31 +0900

    変数名をfilesに変更

--- 08.ls_object/lib/Ls/display.rb
+++ 08.ls_object/lib/Ls/display.rb
@@ -9,7 +9,7 @@ module Ls
     def initialize(options)
       file_names = options['a'] ? Dir.glob('*', ::File::FNM_DOTMATCH) : Dir.glob('*')
       file_names = file_names.reverse if options['r']
-      @file_list = file_names.map { |file_name| Ls::File.new(file_name) }
+      @files = file_names.map { |file_name| Ls::File.new(file_name) }
     end
 
     def show_list_short
@@ -35,20 +35,20 @@ module Ls
     private
 
     def devide_the_file_list
-      num_to_slice = (@file_list.size.to_f / COLUMN_NUMBER).ceil
-      devided_list = @file_list.each_slice(num_to_slice).to_a
+      num_to_slice = (@files.size.to_f / COLUMN_NUMBER).ceil
+      devided_list = @files.each_slice(num_to_slice).to_a
       devided_list.last << nil while devided_list.last.size < num_to_slice
       devided_list
     end
 
     def calc_max_file_name_length
-      @file_list.max_by(&:name_length).name_length
+      @files.max_by(&:name_length).name_length
     end
 
     def build_file_stats_format_list
       max_length_map = build_max_length_map
 
-      @file_list.map do |file|
+      @files.map do |file|
         [
           file.combine_ftype_and_mode,
           file.nlink.to_s.rjust(max_length_map[:nlink] + 1),
@@ -63,15 +63,15 @@ module Ls
 
     def build_max_length_map
       {
-        nlink: @file_list.map { |file| file.nlink.to_s.size }.max,
-        user_name: @file_list.map { |file| file.user_name.size }.max,
-        group_name: @file_list.map { |file| file.group_name.size }.max,
-        size: @file_list.map { |file| file.size.to_s.size }.max
+        nlink: @files.map { |file| file.nlink.to_s.size }.max,
+        user_name: @files.map { |file| file.user_name.size }.max,
+        group_name: @files.map { |file| file.group_name.size }.max,
+        size: @files.map { |file| file.size.to_s.size }.max
       }
     end
 
     def calc_total_blocks
-      @file_list.map { |file| file.blocks }.sum
+      @files.map { |file| file.blocks }.sum
     end
   end
 end
